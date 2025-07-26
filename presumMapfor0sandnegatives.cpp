7dd848a005394441d4a8a35225884e1ccf8d4a5a
HarshvardhanSrivastavaEEE <harshwardhansrivastava09@gmail.com>
2025-07-26 21:08:19 +0530

    Add files via upload
    works for both negatives and zeroes as well

---
+++ presumMapfor0sandnegatives.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Length(vector<int> &array, long long k);
+int main()
+{
+  int size;
+  int k;
+  cout << "Hello! \n";
+  cout << "Please enter the size of your array! \n";
+  cin >> size;
+
+  cout << "Please enter your target! \n";
+  do
+  {
+    cin >> k;
+  } while (k < size);
+  vector <int> array(size);
+
+  cout << "Now lets input your elements! \n";
+
+  for(int i = 0; i < array.size(); i++)
+  {
+   cout << "Enter your element number #" << i+1 << ":" << " ";
+   cin >> array[i];
+   cout << '\n';
+
+  }
+
+  cout << "Your elements are: \n";
+  for(auto it : array)
+  {
+    cout << it << " ";
+
+  }
+  cout << '\n';
+  cout << "Now lets find the max length of your subarray whose sum equals to " << k;
+  cout << '\n';
+  cout << "Max length is: \n";
+  Length(array,k);
+  
+
+
+}
+
+void Length(vector <int> &array, long long k)
+{
+  int maxlen = 0;
+  long long sum = 0;
+  unordered_map <long long , int> presumMap;
+
+  for(int i = 0; i < array.size(); i++)
+  {
+   sum += array[i];
+
+   if(sum == k)
+   {
+     maxlen = max(maxlen,i+1);//checks if new value is greater than previous value
+   }
+    
+   long long rem = sum - k;
+   if(presumMap.find(rem) != presumMap.end())
+   {
+     int len = i - presumMap[rem];
+     maxlen = max(maxlen,len);
+
+   }
+   if(presumMap.find(sum) == presumMap.end())
+   {
+      presumMap[sum] = i;
+   }
+
+  }
+
+cout << maxlen;
+
+}
